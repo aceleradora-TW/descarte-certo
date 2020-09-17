96f88b33d294a22299fb22e3e67500588bd305e5
nicoledbianchin <nicoledbianchin@gmail.com>
2020-09-17 09:51:04 -0300

    :wrench:

--- backend/src/main/java/com/thoughtworks/aceleradora/domain/Encoding.java
+++ backend/src/main/java/com/thoughtworks/aceleradora/domain/Encoding.java
@@ -35,6 +35,7 @@ public final class Encoding {
     public String toString() {
         return "Encoding{" +
                 "original='" + original + '\'' +
+
                 ", encoded='" + encoded + '\'' +
                 '}';
     }
