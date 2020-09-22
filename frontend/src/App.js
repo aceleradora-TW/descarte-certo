2f822c81214663e8f89933bef7ef1af236b6e379
Elliens <helenakray43@gmail.com>
2020-09-22 10:22:53 -0300

    #12/ @isadoramz @elliens: Adiciona POC do React: Página de boas vindas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import {
-  Switch,
-  Route,
-} from "react-router-dom";
-import './App.css';
-import Header from './components/header';
-import Encode from './modules/encode';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header";
+import Encode from "./modules/encode";
+import { Jumbotron } from "react-bootstrap";
 
 function App() {
-  const headerProps = {
-    title: 'Template Aceleradora'
-  };
   return (
-    <div className="App">
-      <Header {...headerProps} />
-
-      <main className="App-content">
-        <Switch>
-          <Route path="/" component={Encode} exact />
-          <Route component={Encode} />
-        </Switch>
-      </main>
+    <div className="flex-box container-box">
+      <Jumbotron className="content-box">
+        <h1>Seja bem vinda, somos o Descarte Certo!</h1>
+        <p>Estamos em construção, em breve o site estará no ar.</p>
+      </Jumbotron>
     </div>
   );
 }
