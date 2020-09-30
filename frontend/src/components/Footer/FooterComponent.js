822cc013c8f5bb541e28f937f83e4e92ca848cac
Rosa Freire <perfildarosa@gmail.com>
2020-09-30 11:52:02 -0300

    #22/ @Vanderlei @Rosa: Adiciona links funcionais

--- frontend/src/components/Footer/FooterComponent.js
+++ frontend/src/components/Footer/FooterComponent.js
@@ -6,6 +6,7 @@ import {FacebookIcon} from '../images/IconsComponents.js'
 import {InstagramIcon} from '../images/IconsComponents.js'
 import {WhatsAppIcon} from '../images/IconsComponents.js' 
 import {EMailIcon} from '../images/IconsComponents.js'  
+import { Nav } from 'react-bootstrap'
 
 
 function FooterComponent(){
@@ -20,10 +21,10 @@ function FooterComponent(){
             <Col className="footerStyle-texts">Telefone: (51) 99851 - 7154</Col>
             
             <Col className="social-media-icons" >
-                <FacebookIcon></FacebookIcon>
-                <InstagramIcon></InstagramIcon>
-                <WhatsAppIcon></WhatsAppIcon>
-                <EMailIcon></EMailIcon>
+                <Nav.Link  target="_blank" href="https://www.facebook.com/5marias.sustentaveis"><FacebookIcon></FacebookIcon></Nav.Link>
+                <Nav.Link  target="_blank" href="https://www.instagram.com/_5marias_/"><InstagramIcon></InstagramIcon></Nav.Link>
+                <Nav.Link  target="_blank" href="whatsapp://send?text=SUA_MENSAGEM_AQUI&phone=+5551998517154+"><WhatsAppIcon></WhatsAppIcon></Nav.Link>
+                <Nav.Link  target="_blank" href="mailto:s.sustentaveis@gmail.com "><EMailIcon></EMailIcon></Nav.Link>
             </Col>
         </Row>
                 
