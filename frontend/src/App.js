f2a254ea6f6680f885e30ee334f8b74fa544e18f
Isadora Zandonotto <isadoraimz@gmail.com>
2020-10-01 12:13:12 -0300

    #23/@isadoramz @alinenecchi: adicionando componente Beneficios com sua primeira coluna pronta na pagina App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Menu from './components/NavBar/index'
 import { Jumbotron } from "react-bootstrap";
 import Footer from './components/Footer/index'
 import Banner from'./components/Banner/index'
+import Benefits from './components/Benefits/index'
 
 function App() {
   const headerProps = {
@@ -12,17 +13,16 @@ function App() {
   return (
     <div className="App">
       <main className="App-content">
+        <div>
           <Menu></Menu>
           <Banner></Banner>
-          
           <div className="flex-box container-box">
           <Jumbotron className="content-box">
+            <Benefits></Benefits>
           </Jumbotron>
          </div>
-         
-          <Footer></Footer>
-          
-
+        </div>
+        <Footer></Footer>
       </main>
     </div>
      
