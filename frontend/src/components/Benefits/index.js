06b73ce6efc3ef55d493db7d152ed7f60799f6a7
Isadora Zandonotto <isadoraimz@gmail.com>
2020-10-01 12:11:56 -0300

    #23/@isadoramz @alinenecchi: adicionando componente Beneficios e sua pagina index.js e index.css

---
+++ frontend/src/components/Benefits/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Benefits() {
+    return(
+        <div>
+            <div>
+                <h1>Benefícios da Plataforma</h1>
+            </div>
+            <div className="row">
+                <div className="col-6" >
+                    <div>
+                        <h3>Garantia do descarte certo</h3>
+                        <p>Todo resíduo que é coletado através de nossos serviços tem a garantia da destinação final correta e adequada, realizada apenas em locais licenciados e regularizados pelos órgãos ambientais responsáveis.</p>
+                    </div>
+                    <div>
+                        <h3>Limpeza Urbana </h3>
+                        <p>Garantindo que o descarte seja feito apenas em locais corretos, nós estamos evitando que estes resíduos sejam descartados em vias públicas ou em áreas impróprias da cidade,  contribuindo assim, com a limpeza urbana e sua manutenção</p>
+                    </div>
+                    <div>
+                        <h3>Economia Circular </h3>
+                        <p>Com a utilização e retorno dos resíduos em novas construções, estamos colocando em prática o conceito de economia circular, que baseia-se na redução, reutilização, recuperação e reciclagem de materiais e energia, prolongando a vida útil destes recursos.</p>
+                    </div>
+                </div>
+                <div className="col-6" >
+                    <h1>Benefícios</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Benefits;
