ba79a7f39fc135036231a9b1d63b7d046ab79c1f
Isadora Zandonotto <isadoraimz@gmail.com>
2020-10-01 13:59:05 -0300

    #23/@isadoramz @alinenecchi: ajustando componentes da paigna principal, adicionando coluna dos beneficios e ajustando css do componente beneficios

--- frontend/src/components/Benefits/index.js
+++ frontend/src/components/Benefits/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './index.css'
 
 function Benefits() {
     return(
         <div>
-            <div>
-                <h1>Benefícios da Plataforma</h1>
+            <div >
+                <h1 className="title">Benefícios da Plataforma</h1>
             </div>
             <div className="row">
                 <div className="col-6" >
@@ -19,10 +20,17 @@ function Benefits() {
                     <div>
                         <h3>Economia Circular </h3>
                         <p>Com a utilização e retorno dos resíduos em novas construções, estamos colocando em prática o conceito de economia circular, que baseia-se na redução, reutilização, recuperação e reciclagem de materiais e energia, prolongando a vida útil destes recursos.</p>
-                    </div>
+                    </div> 
                 </div>
                 <div className="col-6" >
-                    <h1>Benefícios</h1>
+                    <div>
+                        <h3>Preservação Ambiental</h3>
+                        <p>Através da reciclagem dos resíduos coletados e a reinserção na cadeia de produção, estamos contribuindo com a diminuição de extração de novos recursos naturais.</p>
+                    </div>
+                    <div>
+                        <h3>Reciclagem</h3>
+                        <p>Nosso serviço conta com parceiros que trabalham com o beneficiamento e a reciclagem dos resíduos da construção civil coletados por nós. O processo da reciclagem garante o retorno destes resíduos a cadeia produtiva no setor da construção, como material chamado agregado reciclado. </p>
+                    </div>
                 </div>
             </div>
         </div>
