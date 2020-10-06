0264d00bd9be96c504de2538786dcf3efcb9aadf
vanderleijrs <vanderleijrs1977@gmail.com>
2020-10-06 10:30:12 -0300

    #22/@vanderlei@rosa organizando o código

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,31 +2,27 @@ import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import footerStyle from './FooterStyle.css'
-import {Nav} from 'react-bootstrap'
 import FaceIcon from '../images/icon-face.png'
 import InstaIcon from '../images/icon-insta.png'
 import WhatsIcon from '../images/icon-whats.png'
 import MailIcon from '../images/icon-mail.png'
-
-
-
 function FooterComponent(){
         return (
             <div id ="anchorContact"className="footerStyle" fixed="bottom">
                 <Row>
-                <Col>
-                    <Col>Contato:</Col>
-                    <Col className="footerStyle-texts">Telefone: (51) 99851 - 7154</Col>
-                </Col>
-                <Col>
-                    <Col>Redes Sociais:</Col>
-                    <Col className="social-media-icons" >
-                        <a  target="_blank" href="https://www.facebook.com/5marias.sustentaveis"><img src={FaceIcon} width="50px"></img></a>
-                        <a target="_blank" href="https://www.instagram.com/_5marias_/"><img src={InstaIcon} width="50px"></img></a>
-                        <a  target="_blank" href="whatsapp://send?text=SUA_MENSAGEM_AQUI&phone=+5551998517154+"><img src={WhatsIcon} width="50px"></img></a>
-                        <a target="_blank" href="mailto:sustentaveis@gmail.com "><img src={MailIcon} width="50px"></img></a>
+                    <Col>
+                        <Col>Contato:</Col>
+                        <Col className="footerStyle-texts">Telefone: (51) 99851 - 7154</Col>
                     </Col>
-                </Col>  
+                    <Col>
+                        <Col>Redes Sociais:</Col>
+                        <Col className="social-media-icons" >
+                            <a  target="_blank" href="https://www.facebook.com/5marias.sustentaveis"><img src={FaceIcon} width="50px"></img></a>
+                            <a target="_blank" href="https://www.instagram.com/_5marias_/"><img src={InstaIcon} width="50px"></img></a>
+                            <a  target="_blank" href="whatsapp://send?text=SUA_MENSAGEM_AQUI&phone=+5551998517154+"><img src={WhatsIcon} width="50px"></img></a>
+                            <a target="_blank" href="mailto:sustentaveis@gmail.com "><img src={MailIcon} width="50px"></img></a>
+                        </Col>
+                    </Col>  
                 </Row>
             </div>
         )
--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import {Navbar,Nav} from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import NavigationStyles from "./NavBarStyle.css"
 import Logotipo from '../images/logo-5marias.png'
-function NavBarComponent(){
-        return (
-            <Navbar className= "NavBar" bg="light"  expand="lg"  sticky="top"  >
-                <img src={Logotipo} width="200px"></img>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav " />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto mx-auto nb-cont">
-                            <Nav.Link className="nb" href="#anchorContact" >Contato</Nav.Link >
-                            <Nav.Link className="nb" href="#anchorBenefits" >Benefícios</Nav.Link>
-                            <Nav.Link className="nb" href="#anchorTestimonies">Depoimento</Nav.Link>
-                            <Nav.Link className="nb" href="#anchorFAQ">FAQ</Nav.Link>
-                            <Nav.Link className="nb" href="#anchorBudget">Orçamento</Nav.Link>
-                     </Nav>   
-                    </Navbar.Collapse>
-            </Navbar>
-        )
+
+function NavBarComponent() {
+    return (
+        <Navbar className="NavBar" bg="light" expand="lg" sticky="top"  >
+            <img src={Logotipo} width="200px"></img>
+            <Navbar.Toggle aria-controls="basic-navbar-nav " />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto mx-auto nb-cont">
+                    <Nav.Link className="nb" href="#anchorContact" >Contato</Nav.Link >
+                    <Nav.Link className="nb" href="#anchorBenefits" >Benefícios</Nav.Link>
+                    <Nav.Link className="nb" href="#anchorTestimonies">Depoimento</Nav.Link>
+                    <Nav.Link className="nb" href="#anchorFAQ">FAQ</Nav.Link>
+                    <Nav.Link className="nb" href="#anchorBudget">Orçamento</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
 export default NavBarComponent
