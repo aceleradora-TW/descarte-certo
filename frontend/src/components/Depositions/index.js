4302914f6b79dd16fb908f6d88467e724cc37488
AlineNecchi <alinenecchi@gmail.com>
2020-10-27 13:57:22 -0300

    @jhuan0999 @alinenecchi Alterando arquivo para slides

--- frontend/src/components/Depositions/index.js
+++ frontend/src/components/Depositions/index.js
@@ -2,56 +2,89 @@ import React from 'react'
 import './DepositionsStyles.css'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
-import Avatar from '../images/icon-avatar.png'
-
-
+import PhotoCard1 from '../images/img1.png'
+import PhotoCard2 from '../images/img2.png'
+import PhotoCard3 from '../images/img3.png'
+import PhotoCard4 from '../images/img4.png'
+import PhotoCard5 from '../images/img5.png'
+import PhotoCard6 from '../images/img6.png'
+import Carousel from 'react-elastic-carousel'
+import TextsDepositions from './texts.json'
 
 function DepositionsComponent() {
+  
   return (
-    <div>
-      <div className="div-depositions" id="Depoimentos">
-        <h1>Depoimentos</h1>
+    
+  <div className="div-depositions" id="Depoimentos">
+    <div >
+      <h1 >{TextsDepositions.depositions.title}</h1>
+    </div>
+    <Carousel>
+      <CardDeck>
+          <Card>
+            <div  className="imgCard">
+              <Card.Img variant="top" src={PhotoCard1}/>
+            </div>
+            <Card.Body>
+              <Card.Title className='card.title'>{TextsDepositions.card1.title}</Card.Title>
+              <Card.Text className='text-card'>{TextsDepositions.card1.content}</Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <div  class="imgCard">
+              <Card.Img variant="top" src={PhotoCard2}/>
+            </div>
+            <Card.Body>
+              <Card.Title>{TextsDepositions.card2.title}</Card.Title>
+              <Card.Text>{TextsDepositions.card2.content}</Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <div  class="imgCard">
+              <Card.Img variant="top" src={PhotoCard3}/>
+            </div>
+            <Card.Body>
+              <Card.Title>{TextsDepositions.card3.title}</Card.Title>
+              <Card.Text>{TextsDepositions.card3.content}</Card.Text>
+            </Card.Body>
+          </Card>
+      </CardDeck>
+
       <CardDeck>
         <Card>
-          <div  class="avatar">
-            <Card.Img variant="top" src={Avatar}/>
+          <div class="imgCard">
+            <Card.Img variant="top" src={PhotoCard4}/>
           </div>
           <Card.Body>
-              <Card.Title> Sebastian Pereira </Card.Title>
-                <Card.Text>
-                Essa é a importância dos centros de reciclagem, o que antes ia para o aterro vira outro 
-                produto (…) a plataforma da 5 Marias é como um braço comercial da empresa, mesmo sem ser.
-                </Card.Text>
+            <Card.Title>{TextsDepositions.card4.title}</Card.Title>
+            <Card.Text>{TextsDepositions.card4.content}</Card.Text>
           </Card.Body>
         </Card>
 
         <Card>
-          <div  class="avatar">
-            <Card.Img variant="top" src={Avatar}/>
+          <div class="imgCard">
+            <Card.Img variant="top" src={PhotoCard5}/>
           </div>
           <Card.Body>
-              <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
+            <Card.Title>{TextsDepositions.card5.title}</Card.Title>
+            <Card.Text>{TextsDepositions.card5.content}</Card.Text>
           </Card.Body>
         </Card>
 
         <Card>
-          <div  class="avatar">
-            <Card.Img variant="top" src={Avatar}/>
+          <div class="imgCard">
+            <Card.Img variant="top" src={PhotoCard6}/>
           </div>
           <Card.Body>
-              <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
+            <Card.Title>{TextsDepositions.card6.title}</Card.Title>
+            <Card.Text>{TextsDepositions.card6.content}</Card.Text>
           </Card.Body>
         </Card>
       </CardDeck>
-    </div>
-    </div>
+    </Carousel>
+  </div>
+   
   )
 } export default DepositionsComponent
