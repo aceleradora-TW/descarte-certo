5ea81aded8aaed3085194d7aa7d7c48c412609ac
AlineNecchi <alinenecchi@gmail.com>
2020-10-27 19:00:41 -0300

    Testando possiveis formas de implementar carousel responsivo

--- frontend/src/components/Depositions/index.js
+++ frontend/src/components/Depositions/index.js
@@ -14,8 +14,90 @@ import TextsDepositions from './texts.json'
 function DepositionsComponent() {
   
   return (
+  <div className='container-fluid'>
+    <div >
+        <h1 id ="Beneficios" className="title">Benefícios da Plataforma</h1>
+    </div>
+    <Carousel className="card-carousel">
+        <div className="row">
+            <div className="col-sx">
+              <Card>
+                <div class="imgCard">
+                  <Card.Img variant="top" src={PhotoCard1}/>
+                </div>
+                <Card.Body>
+                  <Card.Title className='card.title'>{TextsDepositions.card1.title}</Card.Title>
+                  <Card.Text className='text-card'>{TextsDepositions.card1.content}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="col-sx">
+              <Card>
+                <div class="imgCard">
+                  <Card.Img variant="top" src={PhotoCard2}/>
+                </div>
+                <Card.Body>
+                  <Card.Title className='card.title'>{TextsDepositions.card2.title}</Card.Title>
+                  <Card.Text className='text-card'>{TextsDepositions.card2.content}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="col-sx">
+              <Card>
+                <div class="imgCard">
+                  <Card.Img variant="top" src={PhotoCard3}/>
+                </div>
+                <Card.Body>
+                  <Card.Title className='card.title'>{TextsDepositions.card3.title}</Card.Title>
+                  <Card.Text className='text-card'>{TextsDepositions.card3.content}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-sx">
+              <Card>
+                <div class="imgCard">
+                  <Card.Img variant="top" src={PhotoCard4}/>
+                </div>
+                <Card.Body>
+                  <Card.Title className='card.title'>{TextsDepositions.card4.title}</Card.Title>
+                  <Card.Text className='text-card'>{TextsDepositions.card4.content}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="col-sx">
+              <Card>
+                <div class="imgCard">
+                  <Card.Img variant="top" src={PhotoCard5}/>
+                </div>
+                <Card.Body>
+                  <Card.Title className='card.title'>{TextsDepositions.card5.title}</Card.Title>
+                  <Card.Text className='text-card'>{TextsDepositions.card5.content}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="col-sx">
+              <Card>
+                <div class="imgCard">
+                  <Card.Img variant="top" src={PhotoCard6}/>
+                </div>
+                <Card.Body>
+                  <Card.Title className='card.title'>{TextsDepositions.card6.title}</Card.Title>
+                  <Card.Text className='text-card'>{TextsDepositions.card6.content}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+        </div>
+    </Carousel>
+  </div>
     
-  <div className="div-depositions" id="Depoimentos">
+  /*<div className="div-depositions" id="Depoimentos">
     <div >
       <h1 >{TextsDepositions.depositions.title}</h1>
     </div>
@@ -84,7 +166,7 @@ function DepositionsComponent() {
         </Card>
       </CardDeck>
     </Carousel>
-  </div>
+  </div>*/
    
   )
 } export default DepositionsComponent
