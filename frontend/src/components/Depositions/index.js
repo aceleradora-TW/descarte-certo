b9b79ecda2b01a7ef5fae54d2a5d9ccf1920b588
AlineNecchi <alinenecchi@gmail.com>
2020-10-29 16:37:25 -0300

    Mais ajustes de responsividade

--- frontend/src/components/Depositions/index.js
+++ frontend/src/components/Depositions/index.js
@@ -16,7 +16,7 @@ function DepositionsComponent() {
   }
 function renderCard() {
          
-         if(window.innerWidth < 700){
+         if(window.innerWidth < 600){
          
           return(
             <div id ="Depoimentos" className='container-fluid'>
