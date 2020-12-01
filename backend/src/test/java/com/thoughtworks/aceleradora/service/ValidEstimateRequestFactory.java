314e5ee9baf180667cccfa9da02ce5139d4f2f11
vanderleijrs <vanderleijrs1977@gmail.com>
2020-12-01 14:23:03 -0300

    #28/@jhuan0999 @rosafreire @vanderleijrs: Configura testes backend

--- backend/src/test/java/com/thoughtworks/aceleradora/service/ValidEstimateRequestFactory.java
+++ backend/src/test/java/com/thoughtworks/aceleradora/service/ValidEstimateRequestFactory.java
@@ -3,6 +3,7 @@ package com.thoughtworks.aceleradora.service;
 import com.thoughtworks.aceleradora.controller.request.EstimateRequest;
 import com.thoughtworks.aceleradora.controller.request.RequesterRequest;
 import com.thoughtworks.aceleradora.controller.request.ResidueAddressRequest;
+import com.thoughtworks.aceleradora.controller.request.ResidueRequest;
 
 public class ValidEstimateRequestFactory {
 
@@ -18,6 +19,10 @@ public class ValidEstimateRequestFactory {
                 .residueAddress(ResidueAddressRequest.builder()
                         .cep("900000000")
                         .locationInfo("Tem Elevador")
+                        .build())
+                .residueRequest(ResidueRequest.builder()
+                        .residueType("caliça")
+                        .residueMensure("43m³")
                         .build())
                 .build();
         return estimateRequest;
