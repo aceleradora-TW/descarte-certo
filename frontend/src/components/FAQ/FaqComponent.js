93e687a113a1e32aa5c14b7c27f094334ceb8d74
HodaraMachado <hodaramachado@gmail.com>
2020-12-03 13:49:48 -0300

    #77 @EduardaSchuler @Hodara Machado: Ajuste icone seta em todas as cads

--- frontend/src/components/FAQ/FaqComponent.js
+++ frontend/src/components/FAQ/FaqComponent.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import { Accordion, Card, Button, AccordionContext, } from "react-bootstrap";
 import Icone from '../images/icon-faq-novo.png'
 import imgFaq from '../images/img-faq.png'
@@ -10,23 +10,23 @@ import './FaqResponsive.css'
 import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 import { Link } from 'react-router-dom';
 
-function AccordionToggleButton ({children,eventKey,callback}){
+function AccordionToggleButton({ children, eventKey, callback }) {
     const currentEventKey = useContext(AccordionContext);
 
 
-  const decoratedOnClick = useAccordionToggle(
-    eventKey,
-    () => callback && callback(eventKey),
-  );
+    const decoratedOnClick = useAccordionToggle(
+        eventKey,
+        () => callback && callback(eventKey),
+    );
 
-  const isCurrentEventKey = currentEventKey === eventKey;
-  
-  console.log({currentEventKey,eventKey});
-  return (
-      <Button variant= "link" onClick={decoratedOnClick}>
-      {children} <img src={Icone} className={'iconFaq' + (isCurrentEventKey ? ' iconFaqActive' : '' )}  /> 
-      </Button>
-  );
+    const isCurrentEventKey = currentEventKey === eventKey;
+
+    console.log({ currentEventKey, eventKey });
+    return (
+        <Button variant="link" onClick={decoratedOnClick}>
+            {children} <img src={Icone} className={'iconFaq' + (isCurrentEventKey ? ' iconFaqActive' : '')} />
+        </Button>
+    );
 
 }
 
@@ -37,73 +37,65 @@ function FaqComponent() {
             <Accordion >
                 <Card>
                     <Card.Header>
-                            <AccordionToggleButton eventKey="0">
-                            {Texts.card1.title} 
+                        <AccordionToggleButton eventKey="0">
+                            {Texts.card1.title}
+                        </AccordionToggleButton>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body classname="body-faq">{Texts.card1.content}</Card.Body>
+                    </Accordion.Collapse>
+
+                </Card>
+                <Card>
+                    <Card.Header>
+                        < AccordionToggleButton eventKey="1">
+                            {Texts.card2.title}
+                        </AccordionToggleButton>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                        <Card.Body classname="body-faq">{Texts.card2.content}</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+
+                <Card>
+                    <Card.Header>
+                        <AccordionToggleButton eventKey="3">
+                            {Texts.card3.title}
+                        </AccordionToggleButton>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="3">
+                        <Card.Body classname="body-faq">{Texts.card4.content}</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        < AccordionToggleButton eventKey="4"> 
+                            {Texts.card4.title} 
                             </AccordionToggleButton>
                             </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body classname="body-faq">{Texts.card1.content}</Card.Body>
-                            </Accordion.Collapse>
-                   
-                </Card>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="" eventKey="1">
-                            {Texts.card2.title} <img src={Icone} className="iconFaq" />
-                            <Accordion.Collapse eventKey="1">
-                                <Card.Body classname="body-faq">{Texts.card2.content}</Card.Body>
-                            </Accordion.Collapse>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                </Card>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="" eventKey="2">
-                            {Texts.card3.title}  <img src={Icone} className="iconFaq" />
-                            <Accordion.Collapse eventKey="2">
-                                <Card.Body classname="body-faq">{Texts.card3.content}</Card.Body>
-                            </Accordion.Collapse>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                </Card>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="" eventKey="3">
-                            {Texts.card4.title}  <img src={Icone} className="iconFaq" />
-                            <Accordion.Collapse eventKey="3">
-                                <Card.Body classname="body-faq">{Texts.card4.content}</Card.Body>
-                            </Accordion.Collapse>   </Accordion.Toggle>
-                    </Card.Header>
-                </Card>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="" eventKey="4">
-                            {Texts.card5.title}  <img src={Icone} className="iconFaq" />
                             <Accordion.Collapse eventKey="4">
                                 <Card.Body classname="body-faq">{Texts.card5.content}</Card.Body>
                             </Accordion.Collapse>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                </Card>
+                 </Card>
                 <Card>
                     <Card.Header>
-                        <Accordion.Toggle as={Button} variant="" eventKey="5">
-                            {Texts.card6.title}  <img src={Icone} className="iconFaq" />
+                        < AccordionToggleButton eventKey="5"> 
+                            {Texts.card5.title} 
+                            </AccordionToggleButton>
+                            </Card.Header>
                             <Accordion.Collapse eventKey="5">
                                 <Card.Body classname="body-faq">{Texts.card6.content}</Card.Body>
                             </Accordion.Collapse>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                </Card>
+                 </Card>
                 <Card>
                     <Card.Header>
-                        <Accordion.Toggle as={Button} variant="" eventKey="6">
-                            {Texts.card7.title}  <img src={Icone} className="iconFaq" />
-                            <Accordion.Collapse eventKey="6">
-                                <Card.Body classname="body-faq">{Texts.card7.content}</Card.Body>
-                            </Accordion.Collapse>
-                        </Accordion.Toggle>
+                        < AccordionToggleButton eventKey="6">
+                            {Texts.card6.title}
+                        </AccordionToggleButton>
                     </Card.Header>
+                    <Accordion.Collapse eventKey="6">
+                        <Card.Body classname="body-faq">{Texts.card7.content}</Card.Body>
+                    </Accordion.Collapse>
                 </Card>
             </Accordion>
 
