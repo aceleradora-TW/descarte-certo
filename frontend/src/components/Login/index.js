2fd7807925eb1d3e6789926d9b222cda08a9c83a
jessica.freua <jessicafreua@gmail.com>
2021-05-12 14:23:02 -0300

    #122/@JessicaFreua @HodaraMachado:adicionamos link login na barra menu.

---
+++ frontend/src/components/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+
+function Login() {
+    return (
+       <>
+        <Nav.Link href="#Login">Login</Nav.Link>
+       </>
+    )
+}
+export default Login
