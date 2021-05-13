bf6fc3c02d86de667fcab33489389cc2aaedb7ec
HodaraMachado <hodaramachado@gmail.com>
2021-05-13 13:20:27 -0300

    #122/@JessicaFreua @HodaraMachado: Adicionando incons , inputs e botão na tela de login.

--- frontend/src/components/Login/LoginPage.js
+++ frontend/src/components/Login/LoginPage.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import IconUser from '../images/componenteLoginUsuario.png'
+import IconPass from '../images/componenteSenhaUsuario.png'
 
 function LoginPage() {
   return (
     <div>
       
+      <h1> Login </h1>
+      <div>
+        <label htmlFor="user"> <img alt="userIcone" src={IconUser}></img></label>
+        <input id="user" type="text" name="user" autoComplete="off" placeholder="Usuário" />
+      </div>
+      <div>
+        <label htmlFor="password"><img alt="passIcone" src={IconPass}></img></label>
+        <input id="password" type="password" name="password" placeholder="Senha" />
+      </div>
+      <button>Entrar</button>
     </div>
+
   )
 }
 export default LoginPage
