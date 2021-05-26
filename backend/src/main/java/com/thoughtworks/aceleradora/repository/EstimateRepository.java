911c30b1231e98b0e7f516d722b1795e79c065ac
Dine <dine.nfg@gmail.com>
2021-05-26 15:40:01 -0300

    #119 @dinelands @Elliens: criando endpoint para buscar todas as estimativas

--- backend/src/main/java/com/thoughtworks/aceleradora/repository/EstimateRepository.java
+++ backend/src/main/java/com/thoughtworks/aceleradora/repository/EstimateRepository.java
@@ -3,6 +3,8 @@ package com.thoughtworks.aceleradora.repository;
 import com.thoughtworks.aceleradora.entity.Estimate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 @org.springframework.stereotype.Repository
 public interface EstimateRepository extends JpaRepository<Estimate, Integer>{
 
