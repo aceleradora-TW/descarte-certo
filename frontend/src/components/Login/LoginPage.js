02718fe85b3a0c8ca6c25add4bc23f33afb3c54c
HodaraMachado <hodaramachado@gmail.com>
2021-06-01 14:59:55 -0300

    #122/login-administrador/@DanielleMadrid e @HodaraMachado:Adicionando o icone de user no login e criando a rota do loginpage para a home

--- frontend/src/components/Login/LoginPage.js
+++ frontend/src/components/Login/LoginPage.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import IconUser from '../images/icon_person.png'
 import IconPass from '../images/icon_lock.png'
-import './loginStyle.css';
+import IconBack from '../images/iconevoltar.png'
+import { Nav } from 'react-bootstrap'
+import './loginStyle.css'
 
 function LoginPage() {
   return (
-    <div className="container" >
-      <div className="titleLogin">
-        <h1> Login </h1>
-      </div>
-      <div className="userItem">
-        <label htmlFor="user"> <img alt="userIcone" class="imgIcons"  src={IconUser}></img></label>
-        <input id="user" class="border" type="text" name="user" autoComplete="off" placeholder="Usuário" />
-      </div>
-      <div className="passwordItem">
-        <label htmlFor="password"><img alt="passIcone" class="imgIcons"  src={IconPass}></img></label>
-        <input id="password" class="border" type="password" name="password" placeholder="Senha" />
-      </div>
-      <div className="buttonLog">
-       <button type="submit" class="buttonEnter">Entrar</button>
+    <div>
+      <Nav.Link href="/"> <img alt="userIcone" class="imgIcons" src={IconBack}></img></Nav.Link>
+    
+      <div className="container" >
+        <div className="titleLogin">
+          <h1> Login </h1>
+        </div>
+        <div className="userItem">
+          <label htmlFor="user"> <img alt="userIcone" class="imgIcons" src={IconUser}></img></label>
+          <input id="user" class="border" type="text" name="user" autoComplete="off" placeholder="Usuário" />
+        </div>
+        <div className="passwordItem">
+          <label htmlFor="password"><img alt="passIcone" class="imgIcons" src={IconPass}></img></label>
+          <input id="password" class="border" type="password" name="password" placeholder="Senha" />
+        </div>
+        <div className="buttonLog">
+          <button type="submit" class="buttonEnter">Entrar</button>
+        </div>
       </div>
     </div>
   )
--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap'
+import IconUser from '../images/icon_person.png'
 
 function Login() {
     return (
        <>
-        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/login"><img alt="userIcone" class="imgIcons" src={IconUser}></img>Login</Nav.Link>
        </>
     )
 }
