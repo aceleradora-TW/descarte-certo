5de8fecd9eab7f6775166500968980074acd80ee
Dine <dine.nfg@gmail.com>
2021-06-08 15:51:57 -0300

    #122/@Dinelands @HodaraMachado: ajustes no css e LoginPage

--- frontend/src/components/Login/LoginPage.js
+++ frontend/src/components/Login/LoginPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IconUser from '../images/icon_person.png'
 import IconPass from '../images/icon_lock.png'
 import IconBack from '../images/iconevoltar.png';
@@ -10,78 +10,80 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Nav } from 'react-bootstrap';
 
 function LoginPage() {
+  const [user, setUser] = useState({ email: "", password: "" })
 
   let history = useHistory();
 
-  function handleLogin(e) {    
+  function handleLogin(e) {
     e.preventDefault();
-    
-    const userName = document.getElementById('email').value;
-   
-    const password = document.getElementById('password').value; 
-    
-    if (userName === "teste@teste.com.br" && password === "1234") {  
-     
+
+    if (user.email === "teste@teste.com.br" && user.password === "1234") {
+
       toast.success("Login efetuado com sucesso. Você será redirecionado!");
-      setTimeout(function() {
-        history.push("/solicitacoes")      
+      setTimeout(function () {
+        history.push("/solicitacoes")
       }, 4000)
-    
+
     } else {
       toast.error("Usuário ou senha incorreto. Verifique e tente novamente!");
     }
   }
 
+  function handleChange(e) {
+    const { value, name } = e.target
+    setUser({ ...user, [name]: value })
+  }
+
   return (
     <div>
-    <Nav.Link href="/"> <img alt="userIcone" class="imgIcons" src={IconBack}></img></Nav.Link>
-    <div className="container">     
-       <ToastContainer 
-         position="top-center"
-         autoClose={5000}
-         hideProgressBar={false}
-         newestOnTop={false}
-         closeOnClick
-         rtl={false}  
-         pauseOnFocusLoss
-         draggable
-         pauseOnHover
-       />
+      <Nav.Link href="/"> <img alt="userIcone" class="imgIcons" src={IconBack}></img></Nav.Link>
+      <div className="container">
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
 
-       <div className="titleLogin">  
-            <h1> Login </h1>
-       </div>
-      
-       <Form> 
-             
-         <FormGroup>
-           <Row>
-             <Col xs="1">
-               <label htmlFor="user"> <img alt="userIcone" className="imgIcons" src={IconUser}></img></label>
-             </Col>
-             <Col xs="11">
-               <Input className="form-fields" class="border" type="email" name="email" id="email" placeholder="Usuário" required/>
-             </Col>            
-           </Row>
-         </FormGroup>
-         <FormGroup>
-           <Row>
-             <Col xs="1">
-               <label htmlFor="password"><img alt="passIcone" className="imgIcons" src={IconPass}></img></label>
-             </Col>
-             <Col xs="11">
-               <Input className="form-fields" class="border" type="password" name="password" id="password" placeholder="Senha" required/> 
-             </Col>
-           </Row>          
-         </FormGroup>
-         <Row className="btn-login">
-           <div className="buttonLog">
-             <button type="submit" onClick={handleLogin} className="buttonEnter">Entrar</button>
-           </div>
-         </Row>        
-       </Form>
-     </div>
+        <div className="titleLogin">
+          <h1> Login </h1>
+        </div>
+
+        <Form>
+
+          <FormGroup>
+            <Row>
+              <Col xs="1">
+                <label htmlFor="user"> <img alt="userIcone" className="imgIcons" src={IconUser}></img></label>
+              </Col>
+              <Col xs="11">
+                <Input className="form-fields" onChange={handleChange} class="border" type="email" name="email" id="email" placeholder="Usuário" required />
+              </Col>
+            </Row>
+          </FormGroup>
+          <FormGroup>
+            <Row>
+              <Col xs="1">
+                <label htmlFor="password"><img alt="passIcone" className="imgIcons" src={IconPass}></img></label>
+              </Col>
+              <Col xs="11">
+                <Input className="form-fields" onChange={handleChange} class="border" type="password" name="password" id="password" placeholder="Senha" required />
+              </Col>
+            </Row>
+          </FormGroup>
+          <Row className="btn-login">
+            <div className="buttonLog">
+              <button type="submit" onClick={handleLogin} className="buttonEnter">Entrar</button>
+            </div>
+          </Row>
+        </Form>
+      </div>
     </div>
   )
 }
-export default LoginPage 
+export default LoginPage
