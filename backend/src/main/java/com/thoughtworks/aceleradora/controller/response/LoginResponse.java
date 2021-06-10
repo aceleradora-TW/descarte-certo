c3e55b73b32c85ba67b716f441f93b6a0baf2819
estevandh <estevandelabarihenriques@gmail.com>
2021-06-10 11:49:24 -0300

    :121/@Eduardo-LP @estevandh: Coloca os claims do json

--- backend/src/main/java/com/thoughtworks/aceleradora/controller/response/LoginResponse.java
+++ backend/src/main/java/com/thoughtworks/aceleradora/controller/response/LoginResponse.java
@@ -2,22 +2,27 @@ package com.thoughtworks.aceleradora.controller.response;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-
-import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginResponse {
 
 
-    public String returnLogin(@NotNull String email, String password) {
-        //try
+    public String returnLogin(String email, String password) {
+        Map<String, Object> payloadClaims1 = new HashMap<>();
+        payloadClaims1.put("email", email);
+        Map<String, Object> payloadClaims2 = new HashMap<>();
+        payloadClaims2.put("password", password);
+
         if (email.equals("Sonia") && password.equals("1234abc")) {
             Algorithm algorithm = Algorithm.HMAC256("secret");
             String token = JWT.create()
-                    .withIssuer(email)
+                    .withPayload(payloadClaims1)
+                    .withPayload(payloadClaims2)
                     .sign(algorithm);
             return token;
         } else {
-            return "Nao funcionou";
+            return "USER NOT FOUND";
         }
     }
 }
