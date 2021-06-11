8b39b5b9c59d97100881105ac036a6c9f70584ac
estevandh <estevandelabarihenriques@gmail.com>
2021-06-11 11:09:43 -0300

    121/@Eduardo-LP @estevandh: Coloca email correto na response

--- backend/src/main/java/com/thoughtworks/aceleradora/controller/response/LoginResponse.java
+++ backend/src/main/java/com/thoughtworks/aceleradora/controller/response/LoginResponse.java
@@ -7,14 +7,14 @@ import java.util.Map;
 
 public class LoginResponse {
 
-
     public String returnLogin(String email, String password) {
+
         Map<String, Object> payloadClaims1 = new HashMap<>();
         payloadClaims1.put("email", email);
         Map<String, Object> payloadClaims2 = new HashMap<>();
         payloadClaims2.put("password", password);
 
-        if (email.equals("Sonia") && password.equals("1234abc")) {
+        if (email.equals("5marias.sustentaveis@gmail.com") && password.equals("1234abc")) {
             Algorithm algorithm = Algorithm.HMAC256("secret");
             String token = JWT.create()
                     .withPayload(payloadClaims1)
