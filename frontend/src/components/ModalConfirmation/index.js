58a43c799a68b98f83963e36fd84818291a58195
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-06-14 11:11:47 -0300

    #146/@nanifool @liasantos: adiciona estrutura da modal de confirmação

---
+++ frontend/src/components/ModalConfirmation/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ModalConfirmation () {
+  return (
+    <div>
+      <h3>Seu orçamento: <span>R$250,00</span><span>(Gesso)</span>*</h3>
+      <p>Utilizando o serviço da 5 Marias, você garante o descarte correto do seu resíduo.</p>
+      <div>carrossel</div>
+      <p>* Sujeito às taxas de área azul - EPTC, não incluso no valor.</p>
+      <button>Cancelar Orçamento</button>
+      <button>Aprovar e Agendar</button>
+    </div>    
+  )
+}
+export default ModalConfirmation;
