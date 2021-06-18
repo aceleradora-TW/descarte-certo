b203e61714da6a19d1c4f1c3466301cdf2998dc8
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-06-18 11:12:10 -0300

    154/@Eduardo-LP : adiciona componente responsavel pela rota privada

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -8,6 +8,7 @@ import Footer from './components/Footer/index';
 import Benefits from './components/Benefits/index';
 import LoginPage from './components/Login/LoginPage';
 import OrderListComponent from './components/OrderList';
+import PrivateRouterComponent from './components/PrivateRouter/index';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -16,7 +17,7 @@ ReactDOM.render(
       <Route path="./contato" component={Footer} />
       <Route path="/beneficios" component={Benefits} />
       <Route path="/login" component={LoginPage} />
-      <Route path="/orcamentos" component={OrderListComponent} />
+      <PrivateRouterComponent path="/orcamentos" component={OrderListComponent}/>
     </Switch>
 
   </BrowserRouter>,
