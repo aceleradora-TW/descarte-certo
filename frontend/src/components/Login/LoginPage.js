fa58279147c40fcdbfdc1738307d996a332feb9e
Ju Dantas <jucienyds@gmail.com>
2021-06-21 01:38:28 -0300

    @narukaioh: para evitar conflitos, voltei com essa alteração

--- frontend/src/components/Login/LoginPage.js
+++ frontend/src/components/Login/LoginPage.js
@@ -21,7 +21,7 @@ function LoginPage() {
 
       toast.success("Login efetuado com sucesso. Você será redirecionado!");
       setTimeout(function () {
-        history.push("/orcamentos")
+        history.push("/solicitacoes")
       }, 4000)
 
     } else {
--- frontend/src/services/client.js
+++ frontend/src/services/client.js
@@ -1,13 +1,7 @@
 import axios from 'axios'
 
-const token = localStorage.getItem("token-descarte-certo") || ''
-
-
 export const client = axios.create({
-    baseURL: "/",
-    headers: {
-        Authorization: token ? `bearer ${token}` : ''
-    }
+    baseURL: "/"
 })
 
 export const post = (path, payload) => client.post(path, payload).then(res => res.json()).then(res => res.data)
