6366f7101e66c254f519cc099b9692c557fe24ce
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-06-23 16:05:10 -0300

    #153/@Eduardo-LP @SouAline: insert the index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
@@ -10,18 +9,6 @@ import Benefits from "./components/Benefits/index";
 import LoginPage from "./components/Login/LoginPage";
 import OrderListComponent from "./components/OrderList";
 import PrivateRouterComponent from "./components/PrivateRouter/index";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route , HashRouter } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import Footer from './components/Footer/index';
-import Benefits from './components/Benefits/index';
-import LoginPage from './components/Login/LoginPage';
-import OrderListComponent from './components/OrderList';
->>>>>>> 69aa8a04867d0b6fefe57f83fd97e36379643bb5
 
 ReactDOM.render(
   <HashRouter>
@@ -35,13 +22,6 @@ ReactDOM.render(
         component={OrderListComponent}
       />
     </Switch>
-<<<<<<< HEAD
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
-=======
-
-  </HashRouter>,
-  document.getElementById('root'));
-
->>>>>>> 69aa8a04867d0b6fefe57f83fd97e36379643bb5
