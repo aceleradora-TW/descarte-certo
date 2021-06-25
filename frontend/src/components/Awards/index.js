61bc3eec858b2acff88e9fb2add5d56826fbe078
nanifool <luanadarosadasilva920522@gmail.com>
2021-06-25 15:24:07 -0300

    175/@nanifool @edukng: adiciona media query para responsividade

--- frontend/src/components/Awards/index.js
+++ frontend/src/components/Awards/index.js
@@ -12,13 +12,21 @@ function Awards() {
     <section className="awards-section">
       <h3>Nossas Conquistas</h3>
       <div clasName="logos-section">
-        <div className="awards-div" >
-          <img className="awards-logo" src={LogoFounder} alt="Logo tipo do Founder Institute"></img>
-          <img className="awards-logo" src={LogoAgil} alt="Logo tipo da Aceleradora Ágil" ></img>
-          <img className="awards-logo" src={LogoGarage} alt="Logo tipo da Aceleradora Ágil" ></img>
-          <img className="awards-logo" src={logoCreathon} alt="Logo tipo da Aceleradora Ágil" ></img>
-          <img className="awards-logo" width="200px" src={logoGoogle} alt="Logo tipo da Aceleradora Ágil" ></img>
-          <img className="awards-logo" width="200px" src={logoIclei} alt="Logo tipo da Aceleradora Ágil" ></img>
+        <div className="awards-div">
+          <img className="awards-logo"  width="120px" src={LogoFounder} alt="Logo tipo do Founder Institute"></img>
+          <img className="awards-logo"  width="70" src={LogoAgil} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo"  width="130px" src={LogoGarage} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo"  width="80" src={logoCreathon} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo" width="150px" src={logoGoogle} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo" width="115px" src={logoIclei} alt="Logo tipo da Aceleradora Ágil" ></img>
+        </div>
+        <div className="awards-div-mobile" >
+        <img className="awards-logo"  width="120px" src={LogoFounder} alt="Logo tipo do Founder Institute"></img>
+          <img className="awards-logo"  width="70" src={LogoAgil} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo"  width="130px" src={LogoGarage} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo"  width="80" src={logoCreathon} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo" width="150px" src={logoGoogle} alt="Logo tipo da Aceleradora Ágil" ></img>
+          <img className="awards-logo" width="115px" src={logoIclei} alt="Logo tipo da Aceleradora Ágil" ></img>
         </div>
       </div>
    </section>
