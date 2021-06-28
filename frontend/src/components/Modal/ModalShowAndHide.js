90370097a58e5826edcbb24bdd7d07d36e423fb4
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-06-28 15:56:43 -0300

    adiciona componentes para display and hide da modal

---
+++ frontend/src/components/Modal/ModalShowAndHide.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+
+import ModalAcceptance from '../ModalAcceptance';
+import ModalConfirmation from '../ModalConfirmation';
+
+/*const OrderForm = ({ display, onClick }) => {
+  return display ? (
+    <div className="order-form">
+      <p>formulario de orçamentos</p>
+      <input onClick={onClick} type="button" name="confirmOrder" value="Enviar" />
+    </div>
+  ) : null
+}*/
+//Modal confirmation/confirm order
+const ModalConfirmation = ({ display, onClick }) => {
+  return display ? (
+    <div className="modal-container">
+      <input onClick={onClick} type="button" name="btn-approve" value="Aprovar e Agendar" />
+    </div>) : null
+}
+//Modal acceptance/finish order
+const ModalAcceptance = ({ display }) => {
+  return display ? (
+    <div className="acceptance-content">
+    </div>
+  ) : null
+}
+
+const ContentModal = () => {
+
+  const initialState = {
+  /*orderForm: true,*/
+    ModalConfirmation: true,
+    ModalAcceptance: false
+  }
+
+  const [contentControler, setContentControler] = useState(initialState)
+ 
+  const controlDisplayContent = (e) => {
+    const { name } = e.target
+    setContentControler({ ModalConfirmation: true, ModalAcceptance: false })/*TODO: acrescentar orderForm: false,*/
+    setContentControler({ [name]: true })
+  }
+
+  const { confirmOrder, finishOrder } = contentControler 
+
+  return (
+    <div className="content-modal">
+      {/*TODO: acrescentar  <OrderForm display={orderForm} onClick={controlDisplayContent} />*/}
+      <ModalConfirmation display={ModalConfirmation} onClick={controlDisplayContent} />
+      <ModalAcceptance display={ModalAcceptance} />
+    </div>
+  )
+}
+
+export default ModalShowAndHide
