006e36deafae4141ca1fa60762bc384e1ddc20d0
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-07-01 12:23:32 -0300

    #146/@liasantos @nicolasgbdas: adiciona display e onclick na modalconfirmation

--- frontend/src/components/ModalConfirmation/index.js
+++ frontend/src/components/ModalConfirmation/index.js
@@ -5,11 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
-function ModalConfirmation () {
+function ModalConfirmation ({ display, onClick }) {
   // #TODO: refatorar o onclick para ser close e não reload
   function handlecloseWindow() { window.location.reload() };
   //#TODO: substituir os spans pelos endpoints da modal anterior
-  return (
+  return display ? (
     <div className="modal-container">
       <h3 className="total-budget">Seu orçamento: <span>R$250,00</span><span>(Gesso)</span>*</h3>
       <p className="message">Utilizando o serviço da 5 Marias, você garante o descarte correto do seu resíduo.</p>
@@ -32,9 +32,9 @@ function ModalConfirmation () {
       <p className="eptc-taxes">* Sujeito às taxas de área azul - EPTC, não incluso no valor.</p>
       <button className="btn-cancel" onClick={handlecloseWindow} >Cancelar Orçamento</button>
       <button className="btn-cancel-mobile" onClick={handlecloseWindow}>Cancelar <FontAwesomeIcon icon={faTimesCircle} /></button>
-      <button className="btn-approve">Aprovar e Agendar</button>
+      <button className="btn-approve" onClick={onClick}>Aprovar e Agendar</button>
       <button className="btn-approve-mobile">Confirmar <FontAwesomeIcon icon={faCheckCircle} /></button>
     </div>    
-  )
+  ) : null
 }
 export default ModalConfirmation;
