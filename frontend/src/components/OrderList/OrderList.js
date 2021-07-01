5db21f7117034b88eb9909e74bcbbfa6c021b174
edizzera <moraisedi.dev@gmail.com>
2021-07-01 11:03:45 -0300

    #138/@Eduardo-LP, @Edizzera - Faz paginação e centraliza texto da tabela

--- frontend/src/components/OrderList/OrderList.js
+++ frontend/src/components/OrderList/OrderList.js
@@ -1,41 +1,64 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
+import axios from 'axios';
 
 const OrderList = ({ orders }) => {
+  const [order, setOrder] = useState("");
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 5;
+  const pagesVisited = pageNumber * usersPerPage;
+  const pageCount = Math.ceil(orders.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
   
-  return (
-    <table>
+  return (<>
+    <table className="content-table">
       <thead>
         <tr>
+          <th>Data</th>
           <th>Nome</th>
           <th>Telefone</th>
           <th>email</th>
-          <th>Tipo de coleta</th>
           <th>Quantidade</th>
-          <th>Mateirial</th>
+          <th>Material</th>
           <th>Acesso</th>
           <th>Região</th>
-          <th>Data</th>
           <th>Status</th>
         </tr>
       </thead>
       <tbody>
-        {orders.map((order) => (
+        {orders.slice(pagesVisited, pagesVisited + usersPerPage)
+        .map((order) => (
           <tr key={order.id}>
+            <td>{order.creationDate}</td>
             <td>{order.requester.fullName}</td>
             <td>{order.requester.cellphone}</td>
             <td>{order.requester.email}</td>
             <td>{order.residue.residueMeasure}</td>
-            <td>{order.residue.residueMeasure}</td>
             <td>{order.residue.residueType}</td>
             <td>{order.residueAddress.locationInfo}</td>
-            <td>{order.residueAddress.cep}</td>
-            <td>{order.creationDate}</td>
+            <td>{order.residueAddress.region}</td>
             <td>{order.status}</td>
           </tr>
-        ))}
-      </tbody>
+        ))}        
+      </tbody>      
     </table>
+    <ReactPaginate
+        previousLabel={"Anterior"}
+        nextLabel={"Próximo"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+    </>
   );
 };
 export default OrderList;
--- frontend/src/components/OrderList/index.js
+++ frontend/src/components/OrderList/index.js
@@ -1,54 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import OrderList from "./OrderList";
 import axios from "axios";
-import Pagination from "./Pagination";
-// import ReactPaginate from "react-paginate";
+// import NavBarComponent from '../NavBar/index'
+
 
 const OrderListComponent = () => {
   const [orders, setOrders] = useState("");
-  let [pageNumber, setPageNumber] = useState(0);
-
-  useEffect(() => {    
-    const url = new URL(window.location.href.replace("/#",""))
-    const page = url.searchParams.get("page") || 0
-    console.log(page)
-    
-    axios
-      .get(`https://descartecerto.herokuapp.com/estimate/all?page=${page}&totalPage=1`)
-      .then((res) => {
+  const [pageNumber, setPageNumber] = useState(0);    
+      
+  axios
+    .get(`https://descartecerto.herokuapp.com/estimate/all`)
+    .then((res) => {
         setOrders(res.data.content);
         setPageNumber(res.data.totalPages);
-      });
-
-  }, [pageNumber])
-
-  
+      })  
 
   return (
+    <>
+    
     <div className="container-order-list">
       <div className="order-list-title">
-        {orders.length > 0 ? (
-          <>
-            <OrderList orders={orders} />
-            <Pagination pages={pageNumber} />
-            {/* <ReactPaginate
-              previousLabel={"Previous"}
-              nextLabel={"Next"}
-              pageCount={pageCount}
-              onPageChange={changePage}
-              containerClassName={"PaginationBtn"}
-              previousLinkClassName={"previousBtn"}
-              nextLinkClassName={"nextBtn"}
-              disabledClassName={"paginationDisabled"}
-              activeLinkClassName={"paginationActive"}
-            /> */}
-          </>
-        ) : (
+        {orders.length > 0 
+        ? (
+        <OrderList orders={orders} />)
+        : (
           <h2>Não há orçamentos disponíveis</h2>
         )}
       </div>
     </div>
+    </>
   );
 }
 export default OrderListComponent;
