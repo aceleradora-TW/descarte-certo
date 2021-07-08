8d3b655abb88c84532f4160e2a80aa0cac1a4fb7
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-07-08 15:24:11 -0300

    #158/@liasantos @nicolasgbdas: modifica placeholder de 1 para 0

--- frontend/src/components/Modal/OrderForm.js
+++ frontend/src/components/Modal/OrderForm.js
@@ -302,7 +302,7 @@ const OrderForm = (props) => {
               <div id="quantity">
                 <Field
                   type="Number"
-                  placeholder="1"
+                  placeholder="0"
                   min="1"
                   max="500"
                   name="residueAmount"
