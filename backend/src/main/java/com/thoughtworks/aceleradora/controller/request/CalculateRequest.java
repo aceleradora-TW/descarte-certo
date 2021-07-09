8d05ca7d55d47d8220f69e510f64678478044fc9
edukng <eduk.kng@gmail.com>
2021-07-09 15:34:50 -0300

    207/@edukng @Eduardo-LP: Implementa lombok na classe

--- backend/src/main/java/com/thoughtworks/aceleradora/controller/request/CalculateRequest.java
+++ backend/src/main/java/com/thoughtworks/aceleradora/controller/request/CalculateRequest.java
@@ -4,34 +4,19 @@ import com.thoughtworks.aceleradora.service.bagCalculator.FloorAccess;
 import com.thoughtworks.aceleradora.service.bucketcalculator.BucketZones;
 import com.thoughtworks.aceleradora.service.bucketcalculator.Materials;
 import com.thoughtworks.aceleradora.service.estimateCalculator.ServiceType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CalculateRequest {
-
-
     private int amount;
     private FloorAccess access;
     private BucketZones zones;
     private ServiceType type;
     private Materials material;
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public FloorAccess getAccess() {
-        return access;
-    }
-
-    public BucketZones getZones() {
-        return zones;
-    }
-
-    public ServiceType getType() {
-        return type;
-    }
-
-    public Materials getMaterial() {
-        return material;
-    }
 }
