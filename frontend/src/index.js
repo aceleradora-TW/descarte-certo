73e273f28ac89794a4b1dd0f468316cd3b0149bb
Elliens <helenakray43@gmail.com>
2021-07-09 14:11:31 -0300

    @Elliens @Dinelands: Adiciona modulo Tag Manager(GTM)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter, Switch, Route } from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import App from "./App";
-import Footer from "./components/Footer/index";
-import Benefits from "./components/Benefits/index";
-import LoginPage from "./components/Login/LoginPage";
-import OrderListComponent from "./components/OrderList";
-import PrivateRouterComponent from "./components/PrivateRouter/index";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import Footer from './components/Footer/index';
+import Benefits from './components/Benefits/index';
+import LoginPage from './components/Login/LoginPage';
+import OrderListComponent from './components/OrderList';
+import PrivateRouterComponent from './components/PrivateRouter/index';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-TZC8K8B',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <HashRouter>
@@ -23,5 +30,5 @@ ReactDOM.render(
       />
     </Switch>
   </HashRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
