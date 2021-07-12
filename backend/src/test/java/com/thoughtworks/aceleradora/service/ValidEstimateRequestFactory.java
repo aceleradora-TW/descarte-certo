1704cbbb1b34ba2fd26e732adfd75886b0224b49
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-07-12 15:34:30 -0300

    #207/@edukng @Eduardo-LP @jojorel: atualiza testes

--- backend/src/test/java/com/thoughtworks/aceleradora/service/ValidEstimateRequestFactory.java
+++ backend/src/test/java/com/thoughtworks/aceleradora/service/ValidEstimateRequestFactory.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.aceleradora.service;
 
+import com.thoughtworks.aceleradora.controller.request.CalculateRequest;
 import com.thoughtworks.aceleradora.controller.request.EstimateRequest;
 import com.thoughtworks.aceleradora.controller.request.RequesterRequest;
 import com.thoughtworks.aceleradora.controller.request.ResidueAddressRequest;
@@ -23,6 +24,13 @@ public class ValidEstimateRequestFactory {
                 .residueRequest(ResidueRequest.builder()
                         .residueType("caliça")
                         .residueMeasure("43m³")
+                        .build())
+                .calculate(CalculateRequest.builder()
+                        .amount(60)
+                        .access("ELEVATOR")
+                        .zones("DOWNTOWN")
+                        .type("BAG")
+                        .material("WOOD")
                         .build())
                 .build();
         return estimateRequest;
